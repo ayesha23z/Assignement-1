5289ec3013478b4668754996ea5da21824744590
ayesha23z <zafarat@vcu.edu>
2016-02-08 11:40:15 -0500

    Initial Commit

---
+++ Assignement 1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Assignement 1
+//
+//  Created by Ayesha on 2/8/16.
+//  Copyright © 2016 Ayesha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
